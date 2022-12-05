40e40f107f30e012afee848e873c9955510b8c3e
shai-r-technion <shai.r@campus.technion.ac.il>
2022-12-05 13:25:16 +0200

    day 5

---
+++ 2022/05/solution.js
@@ -0,0 +1,35 @@
+const cloneCrates = (a) => a.slice(0).map((b) => b.slice(0));
+
+export function part1({ crates, instructions }) {
+  crates = cloneCrates(crates);
+  for (const inst of instructions)
+    crates[inst.to - 1].push(...crates[inst.from - 1].splice(-inst.count).reverse());
+
+  return crates.map((c) => c.at(-1)).join('');
+}
+
+export function part2({ crates, instructions }) {
+  crates = cloneCrates(crates);
+  for (const inst of instructions)
+    crates[inst.to - 1].push(...crates[inst.from - 1].splice(-inst.count));
+
+  return crates.map((c) => c.at(-1)).join('');
+}
+
+export function prepare(input) {
+  const [crateLines, instructionLines] = input.split('\n\n').map((ls) => ls.split('\n'));
+  const n = (crateLines[0].length + 1) / 4;
+  const crates = Array.from({ length: n }, () => []);
+
+  for (const line of crateLines.slice(0, -1))
+    for (let i = 0; i < n; ++i)
+      if (line[4 * i + 1] !== ' ') crates[i].unshift(line[4 * i + 1]);
+
+  return {
+    crates,
+    instructions: instructionLines.map((l) => {
+      const ns = l.match(/\d+/g).map((i) => parseInt(i));
+      return { count: ns[0], from: ns[1], to: ns[2] };
+    }),
+  };
+}
