74d094d22dc53a09e1347c254885a57de2657d75
shai-r-technion <shai.r@campus.technion.ac.il>
2023-03-14 01:29:55 +0200

    refactor: year and day resolution

--- run.js
+++ run.js
@@ -1,23 +1,50 @@
 #!/usr/bin/env node
 
 import { argv } from 'node:process';
-import { readFile } from 'node:fs/promises';
+import { readdir, readFile } from 'node:fs/promises';
+import { statSync } from 'fs';
 
 const id = (x) => x;
+
+async function resolveDate(argv) {
+  let [, , day, year] = argv;
+
+  day = parseInt(day) || null;
+  year = parseInt(year) || null;
+
+  if (year == null) {
+    const yearDies = await readdir('.');
+    year = Math.max(
+      ...yearDies
+        .filter((f) => statSync(f).isDirectory())
+        .map((f) => parseInt(f))
+        .filter((n) => !Number.isNaN(n)),
+    );
+  }
+  if (day == null) {
+    const dayDirs = await readdir(`./${year}`);
+    day = Math.max(
+      ...dayDirs
+        .filter((f) => statSync(`./${year}/${f}`).isDirectory())
+        .map((f) => parseInt(f))
+        .filter((n) => !Number.isNaN(n)),
+    );
+  }
+
+  return { day, year };
+}
 
 async function main() {
   let testFlagIndex = argv.indexOf('--test');
   if (testFlagIndex === -1) testFlagIndex = argv.indexOf('-t');
   if (testFlagIndex !== -1) argv.splice(testFlagIndex);
 
-  let [, , day, year] = argv;
-  day ??= new Date().getDate().toString();
-  year ??= new Date().getFullYear().toString();
+  const { day, year } = await resolveDate(argv);
 
-  const mod = await import(`./${year}/${day.padStart(2, '0')}/solution.js`);
+  const mod = await import(`./${year}/${String(day).padStart(2, '0')}/solution.js`);
 
   const inputFile = await readFile(
-    `./${year}/${day.padStart(2, '0')}/${testFlagIndex !== -1 ? 'test' : 'input'}.txt`,
+    `./${year}/${String(day).padStart(2, '0')}/${testFlagIndex !== -1 ? 'test' : 'input'}.txt`,
     { encoding: 'utf8' },
   );
 
